ec3bcd5e7c4a2609678a8b00de513605370e00f8
Mihaela Voicu <101694541+vMihaaaa@users.noreply.github.com>
2023-05-06 10:38:54 +0300

    Update serializer.py
    All serializers

--- Application/hr_project/hr/serializer.py
+++ Application/hr_project/hr/serializer.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Materie, An, Grupa, Profesor, Orar, Sala, Zi
-
+from .models import *
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,103 +11,65 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class MaterieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Materie
-        fields = ["url", "nume_materie"]
-
-    def get_fields(self):
-        fields = super().get_fields()  # Python 3 syntax
-        request = self.context.get("request", None)
-        if request and request.user and request.user.is_superuser is False:
-            fields["nume_materie"].read_only = True
-        return fields
+        fields = ["url", "nume", "credite"]
 
 
-class AnSerializer(serializers.HyperlinkedModelSerializer):
+class Materiile_GrupelorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = An
-        fields = ["url", "id_an"]
+        model = Materiile_Grupelor
+        fields = ["url", "Materie", "Grupa"]
 
-    def get_fields(self):
-        fields = super().get_fields()  # Python 3 syntax
-        request = self.context.get("request", None)
-        if request and request.user and request.user.is_superuser is False and request.user.is_staff is True:
-            fields["id_an"].read_only = True
-        return fields
+
+class Note_Materie_StudentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Note_Materie_Student
+        fields = ["url", "Date_Personale", "Materiile_Grupelor"]
 
 
 class GrupaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Grupa
-        fields = ["url", "id_grupa", "an", "grupa"]
-
-    def get_fields(self):
-        fields = super().get_fields()  # Python 3 syntax
-        request = self.context.get("request", None)
-        if request and request.user and request.user.is_superuser is False:
-            fields["id_grupa"].read_only = True
-            fields["an"].read_only = True
-            fields["grupa"].read_only = True
-        return fields
+        fields = ["url", "Specializare", "an", "grupa", "subgrupa"]
 
 
-class SalaSerializer(serializers.HyperlinkedModelSerializer):
+class SpecializareSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Sala
-        fields = ["url", "id_sala", "nume_sala"]
-
-    def get_fields(self):
-        fields = super().get_fields()  # Python 3 syntax
-        request = self.context.get("request", None)
-        if request and request.user and request.user.is_superuser is False:
-            fields["id_sala"].read_only = True
-            fields["nume_sala"].read_only = True
-        return fields
+        model = Specializare
+        fields = ["url", "nume"]
 
 
-class ProfesorSerializer(serializers.HyperlinkedModelSerializer):
+class Date_PersonaleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Profesor
-        fields = ["url", "CNP", "nume_profesor", "prenume_profesor"]
-
-    def get_fields(self):
-        fields = super().get_fields()  # Python 3 syntax
-        request = self.context.get("request", None)
-        if request and request.user and request.user.is_superuser is False:
-            fields["CNP"].read_only = True
-            fields["nume_profesor"].read_only = True
-            fields["prenume_profesor"].read_only = True
-        return fields
+        model = Date_Personale
+        fields = ["url", "cnp", "nume", "sex", "nationalitate", "cetatenie", "telefon", "adresa", "adresa_mail",
+                    "semestru", "an_studiu"]
 
 
-class ZiSerializer(serializers.HyperlinkedModelSerializer):
+class NoutatiSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Zi
-        fields = ["url", "nume_zi"]
-
-    def get_fields(self):
-        fields = super().get_fields()  # Python 3 syntax
-        request = self.context.get("request", None)
-        if request and request.user and request.user.is_superuser is False:
-            fields["nume_zi"].read_only = True
-        return fields
+        model = Noutati
+        fields = ["url", "titlu", "anunt", "adresa_mail"]
 
 
-class OrarSerializer(serializers.HyperlinkedModelSerializer):
+class SemestruSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Orar
-        fields = ["url", "an", "grupa", "zi", "materie", "profesor", "sala", "inceput_ora", "sfarsit_ora"]
+        model = Semestru
+        fields = ["url", "nr_semestru"]
 
-    def get_fields(self):
-        fields = super().get_fields()  # Python 3 syntax
-        request = self.context.get("request", None)
-        if request and request.user and request.user.is_superuser is True:
-            pass
-        else:
-            fields["an"].read_only = True
-            fields["grupa"].read_only = True
-            fields["zi"].read_only = True
-            fields["materie"].read_only = True
-            fields["profesor"].read_only = True
-            fields["sala"].read_only = True
-            fields["inceput_ora"].read_only = True
-            fields["sfarsit_ora"].read_only = True
-        return fields
+
+class An_StudiuSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = An_Studiu
+        fields = ["url", "an"]
+
+
+class MailSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Mail
+        fields = ["url", "titlu","emitator","receptor","continut"]
+
+        
+class Materiile_ProfesoruluiSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Materiile_Profesorului
+        fields = ["url", "materie","CNP_profesor"]
